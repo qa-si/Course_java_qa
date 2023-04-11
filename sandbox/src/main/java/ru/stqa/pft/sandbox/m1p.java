436e3c0828150407cc2252807b66d64a59f82936
qa-si <130176753+qa-si@users.noreply.github.com>
2023-04-11 20:48:02 +0300

    добавлено вычисление площадей

--- sandbox/src/main/java/ru/stqa/pft/sandbox/m1p.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/m1p.java
@@ -2,6 +2,10 @@ package ru.stqa.pft.sandbox;
 
 public class m1p {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		Square square = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + square.l + " = " + square.area());
+
+		Rectangle rectangle = new Rectangle(4, 6);
+		System.out.println("Площадь прямоугольника со сторонами " + rectangle.a + " и " + rectangle.b + " = " + rectangle.area());
 	}
 }
