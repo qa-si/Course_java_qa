0d34532a41191d6d3fbdcb783d2768a810e00ddd
qa-si <130176753+qa-si@users.noreply.github.com>
2023-04-12 10:25:20 +0300

    Скрыл атрибуты класса Point за модификатором доступа private

--- sandbox/src/main/java/ru/stqa/pft/sandbox/CalculateDistanceBetweenPoints.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/CalculateDistanceBetweenPoints.java
@@ -8,11 +8,10 @@ public class CalculateDistanceBetweenPoints {
         Point p2 = new Point(3, 0);
 
         System.out.println("Расстояние между точками " + p1.pointCoordinates() + " и " + p2.pointCoordinates() + " = " + distanceBetweenPoints(p1, p2));
-        System.out.println("Расстояние от точки " + p1.pointCoordinates() + " до точки " + p2.pointCoordinates() + " = " + p1.distanceToPoint(p2.x, p2.y));
+        System.out.println("Расстояние от точки " + p1.pointCoordinates() + " до точки " + p2.pointCoordinates() + " = " + p1.distanceToPoint(p2));
     }
 
     public static double distanceBetweenPoints(Point p1, Point p2){
-        return Math.sqrt(Math.pow(p2.x - p1.x, 2) +Math.pow(p2.y - p1.y, 2));
+        return Math.sqrt(Math.pow(p2.getX() - p1.getX(), 2) +Math.pow(p2.getY() - p1.getY(), 2));
     }
-
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,19 +2,27 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-    public double x;
-    public double y;
+    private double x;
+    private double y;
 
     public Point(double x, double y){
         this.x = x;
         this.y = y;
     }
 
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
     public String pointCoordinates() {
         return "(" + this.x + ";" + this.y + ")";
     }
 
-    public double distanceToPoint(double x, double y){
-        return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
+    public double distanceToPoint(Point p){
+        return Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
     }
 }
