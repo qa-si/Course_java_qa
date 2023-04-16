3d0659c20703471fe01bfa34f6b999cda79f1470
qa-si <130176753+qa-si@users.noreply.github.com>
2023-04-16 21:40:09 +0300

    выделил методы заполнения и клика, класс HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appManager/SessionHelper.java
@@ -4,20 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.LoginData;
 
-public class SessionHelper {
-
-	private WebDriver wd;
+public class SessionHelper extends HelperBase{
 
 	public SessionHelper(WebDriver wd) {
-		this.wd = wd;
+		super(wd);
 	}
 
-
 	public void login(LoginData loginData) {
-		wd.findElement(By.name("user")).clear();
-		wd.findElement(By.name("user")).sendKeys(loginData.getUsername());
-		wd.findElement(By.name("pass")).clear();
-		wd.findElement(By.name("pass")).sendKeys(loginData.getPassword());
-		wd.findElement(By.xpath("//input[@value='Login']")).click();
+		type(By.name("user"), loginData.getUsername());
+		type(By.name("pass"), loginData.getPassword());
+		click(By.xpath("//input[@value='Login']"));
 	}
 }
