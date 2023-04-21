a10e09c319a1c2a47afcdb9e7ad19f163c5a82da
qa-si <130176753+qa-si@users.noreply.github.com>
2023-04-21 18:02:10 +0300

    refactoring if-else

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -6,27 +6,31 @@ public class Equation {
 	private final double c;
 	private int n;
 
-	public Equation(double a, double b, double c){
+	public Equation(double a, double b, double c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 
-		double d = b*b-4*a*a*c;
+		double d = b * b - 4 * a * a * c;
 
-		if(d > 0) {
-			n = 2;
-		}
-		else {
-			if (d == 0){
+		if (a != 0) {
+			if (d > 0) {
+				n = 2;
+			} else if (d == 0) {
 				n = 1;
-			}
-			else {
+			} else {
 				n = 0;
 			}
+		} else if (b != 0) {
+			n = 1;
+		} else if (c != 0) {
+			n = 0;
+		} else {
+			n = -1;
 		}
 	}
 
-	public int rootNumber(){
+	public int rootNumber() {
 		return n;
 	}
 }
