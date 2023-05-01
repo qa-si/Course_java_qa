863bef8a2145b5f7261a7c2b6fc4693caa2f1399
qa-si <130176753+qa-si@users.noreply.github.com>
2023-05-01 13:08:19 +0300

    выбор элемента по индексу

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -14,7 +14,7 @@ public class GroupModificationTests extends TestBase{
 			app.getGroupHelper().createGroup(new GroupData("test10", null, null));
 		}
 		int before = app.getGroupHelper().getGroupCount();
-		app.getGroupHelper().selectGroup();
+		app.getGroupHelper().selectGroup(before-1);
 		app.getGroupHelper().initGroupModification();
 		app.getGroupHelper().fillGroupForm(new GroupData("test100000", "test20000", "test300000"));
 		app.getGroupHelper().submitGroupModification();
