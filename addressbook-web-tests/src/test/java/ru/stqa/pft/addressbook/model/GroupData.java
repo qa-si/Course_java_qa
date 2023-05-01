4695405d7b8ad97a9ef7e9ef7aac34997c647b6b
qa-si <130176753+qa-si@users.noreply.github.com>
2023-05-01 14:04:59 +0300

    лекция 4.8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,38 +4,12 @@ import java.util.Objects;
 
 public class GroupData {
 
-	private final String id;
+	private int id;
 	private final String name;
 	private final String header;
 	private final String footer;
 
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		GroupData groupData = (GroupData) o;
-		return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, name);
-	}
-
-	@Override
-	public String toString() {
-		return "GroupData{" +
-				"id='" + id + '\'' +
-				", name='" + name + '\'' +
-				'}';
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public GroupData(String id, String name, String header, String footer) {
+	public GroupData(Integer id, String name, String header, String footer) {
 		this.id = id;
 		this.name = name;
 		this.header = header;
@@ -43,7 +17,7 @@ public class GroupData {
 	}
 
 	public GroupData(String name, String header, String footer) {
-		this.id = null;
+		this.id = 0;
 		this.name = name;
 		this.header = header;
 		this.footer = footer;
@@ -61,4 +35,32 @@ public class GroupData {
 		return footer;
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		GroupData groupData = (GroupData) o;
+		return id == groupData.id && Objects.equals(name, groupData.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public String toString() {
+		return "GroupData{" +
+				"id='" + id + '\'' +
+				", name='" + name + '\'' +
+				'}';
+	}
 }
