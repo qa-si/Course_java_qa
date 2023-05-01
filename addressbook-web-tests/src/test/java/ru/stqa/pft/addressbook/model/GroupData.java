b23aa283741170dcacd01689e46696d6a9cc111e
qa-si <130176753+qa-si@users.noreply.github.com>
2023-05-01 13:30:50 +0300

    лекция 4.6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,6 +1,9 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class GroupData {
+
 	private final String name;
 	private final String header;
 	private final String footer;
@@ -21,5 +24,25 @@ public class GroupData {
 
 	public String getFooter() {
 		return footer;
+	}
+
+	@Override
+	public String toString() {
+		return "GroupData{" +
+				"name='" + name + '\'' +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		GroupData groupData = (GroupData) o;
+		return Objects.equals(name, groupData.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
 	}
 }
