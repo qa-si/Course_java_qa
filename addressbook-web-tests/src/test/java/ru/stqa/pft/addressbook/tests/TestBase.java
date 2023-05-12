63646165cf380080843e484e21739e7c5af17a31
qa-si <130176753+qa-si@users.noreply.github.com>
2023-05-12 17:58:04 +0300

    изменил аннотацию before

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -2,7 +2,9 @@ package ru.stqa.pft.addressbook.tests;
 
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appManager.ApplicationManager;
 
 import static org.openqa.selenium.remote.Browser.CHROME;
@@ -10,14 +12,14 @@ import static org.openqa.selenium.remote.Browser.EDGE;
 
 public class TestBase {
 
-	protected final ApplicationManager app = new ApplicationManager(CHROME.browserName());
+	protected static final ApplicationManager app = new ApplicationManager(CHROME.browserName());
 
-	@BeforeMethod(alwaysRun = true)
+	@BeforeSuite
 	public void setUp() throws Exception {
 		app.init();
 	}
 
-	@AfterMethod(alwaysRun = true)
+	@AfterSuite
 	public void tearDown() throws Exception {
 		app.stop();
 	}
