fb848560f6bebbfc1eed27140f250df00392b394
qa-si <130176753+qa-si@users.noreply.github.com>
2023-05-17 23:52:02 +0300

    Задание №12: Реализовать генераторы тестовых данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,13 +1,23 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.io.File;
 import java.util.Objects;
 
+@XStreamAlias("group")
 public class ContactData {
+
+	@XStreamOmitField
 	private int id = Integer.MAX_VALUE;
+	@Expose
 	private String name;
+	@Expose
 	private String lastname;
 	private String address = "";
+	@Expose
 	private String email;
 	private String email2;
 	private String email3;
