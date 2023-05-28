13d73db151b85f8638bd1f9a0e957da93412b7c2
qa-si <130176753+qa-si@users.noreply.github.com>
2023-05-28 12:45:22 +0300

    Добавлено логирование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appManager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import static org.openqa.selenium.remote.Browser.CHROME;
 
+
 public class TestBase {
+	Logger logger = LoggerFactory.getLogger(TestBase.class);
 
 	protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", CHROME.browserName()));
 
@@ -16,7 +24,17 @@ public class TestBase {
 		app.init();
 	}
 
-	@AfterSuite
+	@BeforeMethod
+	public void logTestStart(Method m, Object[] p){
+		logger.info("Start test " + m.getName() + "with parameters " + Arrays.asList(p));
+	}
+
+	@AfterMethod(alwaysRun = true)
+	public void logTestStop(Method m){
+		logger.info("Stop test " + m.getName());
+	}
+
+	@AfterSuite(alwaysRun = true)
 	public void tearDown() throws Exception {
 		app.stop();
 	}
