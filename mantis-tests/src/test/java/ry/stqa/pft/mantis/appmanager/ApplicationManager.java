c94280e5f3eb681f0316f8241c3e9fb570e10dbc
qa-si <130176753+qa-si@users.noreply.github.com>
2023-06-03 21:49:46 +0300

    Лекция 8.4

--- mantis-tests/src/test/java/ry/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ry/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
 package ry.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -17,8 +16,9 @@ import static org.openqa.selenium.remote.Browser.*;
 
 public class ApplicationManager {
 	private final Properties properties;
-	WebDriver wd;
+	private WebDriver wd;
 	private final String browser;
+	private RegistrationHelper registrationHelper;
 
 	public ApplicationManager(String browser) {
 		this.browser = browser;
@@ -28,27 +28,41 @@ public class ApplicationManager {
 	public void init() throws IOException {
 		String target = System.getProperty("target", "local");
 		properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-		if (Objects.equals(browser, FIREFOX.browserName())) {
-			wd = new FirefoxDriver();
-		} else if (Objects.equals(browser, CHROME.browserName())) {
-			wd = new ChromeDriver();
-		} else if (Objects.equals(browser, EDGE.browserName())) {
-			wd = new EdgeDriver();
-		}
-		wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-		wd.get(properties.getProperty("web.baseUrl"));
 	}
 
 	public void stop() {
-		wd.quit();
+		if (wd != null) {
+			wd.quit();
+		}
 	}
 
-	public HttpSession newSession(){
+	public HttpSession newSession() {
 		return new HttpSession(this);
 	}
 
-	public String getProperty(String key){
+	public String getProperty(String key) {
 		return properties.getProperty(key);
+	}
+
+	public RegistrationHelper registration() {
+		if(registrationHelper == null){
+			registrationHelper = new RegistrationHelper(this);
+		}
+		return registrationHelper;
+	}
+
+	public WebDriver getDriver() {
+		if (wd == null) {
+			if (Objects.equals(browser, FIREFOX.browserName())) {
+				wd = new FirefoxDriver();
+			} else if (Objects.equals(browser, CHROME.browserName())) {
+				wd = new ChromeDriver();
+			} else if (Objects.equals(browser, EDGE.browserName())) {
+				wd = new EdgeDriver();
+			}
+			wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+			wd.get(properties.getProperty("web.baseUrl"));
+		}
+		return wd;
 	}
 }
