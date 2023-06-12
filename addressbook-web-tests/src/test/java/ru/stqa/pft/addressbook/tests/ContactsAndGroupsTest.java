6bdc84d9060301f56a87ecda6fabbfe2814ad8f3
qa-si <130176753+qa-si@users.noreply.github.com>
2023-06-12 21:46:10 +0300

    Задание №16: Реализовать тесты для добавления контакта в группу и удаления контакта из группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactsAndGroupsTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactsAndGroupsTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
@@ -11,11 +12,34 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactsAndGroupsTest extends TestBase {
 
+	private String groupName = "newTestGroup";
+	private String contactName = "newTestContact";
+	Groups groups;
+	Contacts contacts;
+
+	@BeforeTest
+	public void preparation() {
+		if (app.db().groups().size() == 0) {
+			app.group().create(new GroupData().withName(groupName));
+		}
+		if (app.db().contacts().size() == 0) {
+			app.contact().create(new ContactData().withFirstname(contactName));
+		}
+		groups = app.db().groups();
+		contacts = app.db().contacts();
+		contacts = app.contact().setGroupsToContacts(contacts, groups);
+	}
+
 	@Test()
 	public void testContactAddToGroup() {
-		Groups groups = app.db().groups();
-		Contacts contacts = app.db().contacts();
-		contacts = app.contact().setGroupsToContacts(contacts, groups);
+		for (int i = 0; i < contacts.size(); i++) {
+			ContactData contact = contacts.iterator().next();
+			if (contact.getGroups().size() != groups.size()) {
+				break;
+			} else if (i == contacts.size() - 1) {
+				app.contact().create(new ContactData().withFirstname(contactName));
+			}
+		}
 		ContactData contact = contacts.iterator().next();
 		GroupData addGroup = new GroupData();
 		app.goTo().groupPage();
@@ -36,13 +60,25 @@ public class ContactsAndGroupsTest extends TestBase {
 
 	@Test()
 	public void testContactRemoveFromGroup() {
-		Groups groups = app.db().groups();
-		for (GroupData group : groups) {
-			app.contact().openContactListInGroup(group);
+		GroupData groupForRemoveContact;
+		for (int i = 0; i < contacts.size(); i++) {
+			ContactData contact = contacts.iterator().next();
+			if (contact.getGroups().size() != 0) {
+				break;
+			} else if (i == contacts.size() - 1) {
+				groupForRemoveContact = groups.iterator().next();
+				app.goTo().contactPage();
+				app.contact().selectContactById(contact.getId());
+				app.contact().addContactToGroup(groupForRemoveContact);
+			}
+		}
+		for (int i = 0; i < groups.size(); i++) {
+			groupForRemoveContact = groups.iterator().next();
+			app.contact().openContactListInGroup(groupForRemoveContact);
 			if (app.contact().all().size() > 0) {
 				ContactData contact = app.contact().all().iterator().next();
-				app.contact().removeContactFromGroup(contact, group);
-				app.contact().returnToGroupClick(group);
+				app.contact().removeContactFromGroup(contact, groupForRemoveContact);
+				app.contact().returnToGroupClick(groupForRemoveContact);
 				assertThat(false, equalTo(app.contact().checkContactIsVisible(contact)));
 				break;
 			}
