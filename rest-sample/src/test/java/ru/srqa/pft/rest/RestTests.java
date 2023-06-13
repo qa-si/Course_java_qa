85283d80c84bf1ea5c740fd13941377744145442
qa-si <130176753+qa-si@users.noreply.github.com>
2023-06-13 22:43:52 +0300

    refactoring

--- rest-sample/src/test/java/ru/srqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/srqa/pft/rest/RestTests.java
@@ -1,19 +1,20 @@
 package ru.srqa.pft.rest;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
-import org.testng.Assert;
 import org.testng.SkipException;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.Set;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 public class RestTests {
 
